5c60212cbe6dda08ec1341f6f37c8b90395eb80f
Dan Beglin <dbeglinuk@gmail.com>
2014-11-23 17:09:42 +0000

    Plays multiple games and write to .txt!
    Almost Complete, but uses Recursion, which can error out. 
    
    Will probably turn into while loop if it doesnt sort out before Ive solved it

--- pegSolitareSolver.py
+++ pegSolitareSolver.py
@@ -11,11 +11,12 @@ completes the game. It will then print out the successful game moves to a .txt f
 
 import random
 import time
+import sys
 
 """
 Variable Declarations
 """
-
+sys.setrecursionlimit(30000)
 
 movesPossible = 0
 
@@ -26,12 +27,12 @@ dPicked = 0
 move = 0
 
 gameBoard = [[9,9,1,1,1,9,9,],\
-            [9,9,1,1,1,9,9,],\
-            [1,1,1,1,1,1,1,],\
-            [1,1,1,0,1,1,1,],\
-            [1,1,1,1,1,1,1,],\
-            [9,9,1,1,1,9,9,],\
-            [9,9,1,1,1,9,9,]]
+                 [9,9,1,1,1,9,9,],\
+                 [1,1,1,1,1,1,1,],\
+                 [1,1,1,0,1,1,1,],\
+                 [1,1,1,1,1,1,1,],\
+                 [9,9,1,1,1,9,9,],\
+                 [9,9,1,1,1,9,9,]]
 
 movesList = [[9,9,9],\
                  [9,9,9],\
@@ -85,9 +86,11 @@ movesList = [[9,9,9],\
 
 """
 Functions
+
 """
 
 def resetBoard():
+
     gameBoard = [[9,9,1,1,1,9,9,],\
                  [9,9,1,1,1,9,9,],\
                  [1,1,1,1,1,1,1,],\
@@ -95,7 +98,7 @@ def resetBoard():
                  [1,1,1,1,1,1,1,],\
                  [9,9,1,1,1,9,9,],\
                  [9,9,1,1,1,9,9,]]
-
+    
     return gameBoard
 
 def resetMovesList():
@@ -157,13 +160,11 @@ def checkMoves(movesPossible, gameBoard, movesList):
                 movesList[movesPossible][2] = x
                 movesList[movesPossible][0] = 'h'
                 movesPossible = movesPossible + 1
-                #print movesList[movesPossible][1], movesList[movesPossible][2] #DEBUG
             elif(x + 2 < 7 and gameBoard[y][x] == 0 and gameBoard[y][x + 1] == 1 and gameBoard[y][x + 2] == 1):
                 movesList[movesPossible][1] = y
                 movesList[movesPossible][2] = x
                 movesList[movesPossible][0] = 'h'
                 movesPossible = movesPossible + 1
-                #print movesList[movesPossible][1], movesList[movesPossible][2] #DEBUG
             x = x + 1
         y = y + 1
         
@@ -174,16 +175,13 @@ def checkMoves(movesPossible, gameBoard, movesList):
                 movesList[movesPossible][2] = x
                 movesList[movesPossible][0] = 'v'
                 movesPossible = movesPossible + 1
-                #print movesList[movesPossible][1], movesList[movesPossible][2] #DEBUG
             elif(y + 2 < 7 and gameBoard[y][x] == 0 and gameBoard[y + 1][x] == 1 and gameBoard[y + 2][x] == 1):
                 movesList[movesPossible][1] = y
                 movesList[movesPossible][2] = x
                 movesList[movesPossible][0] = 'v'
                 movesPossible = movesPossible + 1
-                #print movesList[movesPossible][1], movesList[movesPossible][2] #DEBUG
             x = x + 1
         y = y + 1
-    print "movesPossible = ",movesPossible
     
     return movesPossible, gameBoard, movesList
 
@@ -195,35 +193,73 @@ def pickMove(movesPossible, movesList, yPicked, xPicked, dPicked, move):
     movesPossible = 0
     return movesPossible, movesList, yPicked, xPicked, dPicked, move
 
-def executeMove(gameBoard, movesList, yPicked, xPicked, dPicked, move):
+def executeMove(gameBoard, movesList, yPicked, xPicked, dPicked, move, pyPegResults):
+    xPicked2 = xPicked + 2
+    yPicked2 = yPicked + 2
+    xPickedStr = str(xPicked)
+    yPickedStr = str(yPicked)
+    yPicked2Str = str(yPicked2)
+    xPicked2Str = str(xPicked2)
     if(movesList[move][0] == "h"):
         if(gameBoard[yPicked][xPicked] == 0 and gameBoard[yPicked][xPicked + 1] == 1 and gameBoard[yPicked][xPicked + 2] == 1):
             gameBoard[yPicked][xPicked] = 1
             gameBoard[yPicked][xPicked + 1] = 0
             gameBoard[yPicked][xPicked + 2] = 0
-            print "Horizontal Take Right" #DEBUG
+            pyPegResults.write("Move from ")
+            pyPegResults.write(yPickedStr)
+            pyPegResults.write(" , ")
+            pyPegResults.write(xPicked2Str)
+            pyPegResults.write(" to ")
+            pyPegResults.write(yPickedStr)
+            pyPegResults.write(" , ")
+            pyPegResults.write(xPickedStr)
+            pyPegResults.write("\n")
         if(gameBoard[yPicked][xPicked] == 1 and gameBoard[yPicked][xPicked + 1] == 1 and gameBoard[yPicked][xPicked + 2] == 0):
             gameBoard[yPicked][xPicked] = 0
             gameBoard[yPicked][xPicked + 1] = 0
             gameBoard[yPicked][xPicked + 2] = 1
-            print "Horizontal Take Left" #DEBUG
+            pyPegResults.write("Move from ")
+            pyPegResults.write(yPickedStr)
+            pyPegResults.write(" , ")
+            pyPegResults.write(xPickedStr)
+            pyPegResults.write(" to ")
+            pyPegResults.write(xPicked2Str)
+            pyPegResults.write(" , ")
+            pyPegResults.write(yPickedStr)
+            pyPegResults.write("\n")
     elif(movesList[move][0] == "v"):
         if(gameBoard[yPicked][xPicked] == 0 and gameBoard[yPicked + 1][xPicked] == 1 and gameBoard[yPicked + 2][xPicked] == 1):
             gameBoard[yPicked][xPicked] = 1
             gameBoard[yPicked + 1][xPicked] = 0
             gameBoard[yPicked + 2][xPicked] = 0
-            print "Vertical Take Up" #DEBUG
+            pyPegResults.write("Move from ")
+            pyPegResults.write(yPicked2Str)
+            pyPegResults.write(" , ")
+            pyPegResults.write(xPickedStr)
+            pyPegResults.write(" to ")
+            pyPegResults.write(yPickedStr)
+            pyPegResults.write(" , ")
+            pyPegResults.write(xPickedStr)
+            pyPegResults.write("\n")
         if(gameBoard[yPicked][xPicked] == 1 and gameBoard[yPicked + 1][xPicked] == 1 and gameBoard[yPicked + 2][xPicked] == 0):
             gameBoard[yPicked][xPicked] = 0
             gameBoard[yPicked + 1][xPicked] = 0
             gameBoard[yPicked + 2][xPicked] = 1
-            print "Vertical Take Down" #DEBUG
+            pyPegResults.write("Move from ")
+            pyPegResults.write(yPickedStr)
+            pyPegResults.write(" , ")
+            pyPegResults.write(xPickedStr)
+            pyPegResults.write(" to ")
+            pyPegResults.write(yPicked2Str)
+            pyPegResults.write(" , ")
+            pyPegResults.write(xPickedStr)
+            pyPegResults.write("\n")
     else:
         print "ERROR, Neither V nor H in MoveList!"
-    return gameBoard, movesList, yPicked, xPicked, dPicked, move
+    return gameBoard, movesList, yPicked, xPicked, dPicked, move, pyPegResults
 
 
-def evaluateBoard(gameBoard, pegSolved):
+def evaluateBoard(gameBoard, pegSolved, pegsLeft):
     pegsLeft = 0
     for a in range(0,7):
         for b in range(0,7):
@@ -233,8 +269,7 @@ def evaluateBoard(gameBoard, pegSolved):
         a = a + 1
     if(pegsLeft == 1 and gameBoard[3][3] == 1):
         pegSolved == True
-    print"Pegs Left: ",pegsLeft
-    return gameBoard, pegSolved
+    return gameBoard, pegSolved, pegsLeft
         
 
 """
@@ -244,32 +279,47 @@ Actual Sequence
 
 pegSolved = False
 gameFinished = False
+pegsLeft = 0
+pyPegResults = open("pyPegResults.txt","w")
+gamesPlayed = 0
 
-
-def play(pegSolved, gameFinished, movesPossible, gameBoard, movesList, yPicked, xPicked, dPicked, move):
+def play(pegSolved, gameFinished, movesPossible, gameBoard, movesList, yPicked, xPicked, dPicked, move, pegsLeft, pyPegResults, gamesPlayed):
     while pegSolved == False:
+        pyPegResults = open("pyPegResults.txt","w")
+        print"Games Played: ", gamesPlayed
         gameBoard = resetBoard()
-        print"RESETING BOARD"
         resetMovesList()
         gameFinished = False
         while gameFinished == False:
             movesPossible, gamesBoard, movesList = checkMoves(movesPossible, gameBoard, movesList)
             if(movesPossible == 0):
-                print"END"
+                
                 gameFinished = True
                 break
-                print"END OF ROUND"
             movesPossible, movesList, yPicked, xPicked, dPicked, move = pickMove(movesPossible, movesList, yPicked, xPicked, dPicked, move)
-            gameBoard, movesList, yPicked, xPicked, dPicked, move = executeMove(gameBoard, movesList, yPicked, xPicked, dPicked, move)  
-        gameBoard, pegSolved = evaluateBoard(gameBoard, pegSolved)
-        print gameBoard
-        print pegSolved
+            gameBoard, movesList, yPicked, xPicked, dPicked, move, pyPegResults = executeMove(gameBoard, movesList, yPicked, xPicked, dPicked, move, pyPegResults)  
+        gameBoard, pegSolved, pegsLeft = evaluateBoard(gameBoard, pegSolved, pegsLeft)
         time.sleep(2)
-        if(pegSolved == True):
-            print"SUCESS"
-        else:
-            play(pegSolved, gameFinished, movesPossible, gameBoard, movesList, yPicked, xPicked, dPicked, move)
 
+        if(pegsLeft == 1 and gameBoard[3][3] == 1):
+            print"SUCCESS"
+            pyPegResults.write("Game Won!")
+            pyPegResults.write("\n I've Played ")
+            gamesPlayed = gamesPlayed + 1
+            gamesPlayedStr = str(gamesPlayed)
+            pyPegResults.write(gamesPlayedStr)
+            pyPegResults.write(" Games before I solved it!")
+            pyPegResults.close()
+            break
+        else:
+            pyPegResults.write("Game Lost!")
+            pyPegResults.write("\n I've Played ")
+            gamesPlayed = gamesPlayed + 1
+            gamesPlayedStr = str(gamesPlayed)
+            pyPegResults.write(gamesPlayedStr)
+            pyPegResults.write(" Games so far!")
+            pyPegResults.close()
+            play(pegSolved, gameFinished, movesPossible, gameBoard, movesList, yPicked, xPicked, dPicked, move, pegsLeft, pyPegResults, gamesPlayed)
     print "SUCCESS! Check how to Solve Peg Solitare!"
 
-play(pegSolved, gameFinished, movesPossible, gameBoard, movesList, yPicked, xPicked, dPicked, move)
+play(pegSolved, gameFinished, movesPossible, gameBoard, movesList, yPicked, xPicked, dPicked, move, pegsLeft, pyPegResults, gamesPlayed)

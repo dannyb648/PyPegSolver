2615a1e686c456d4a2612a7f4e971f0e3bf37202
Dan Beglin <dbeglinuk@gmail.com>
2014-11-24 12:31:21 +0000

    Update pegSolitareSolver.py

--- pegSolitareSolver.py
+++ pegSolitareSolver.py
@@ -3,261 +3,334 @@
 Created on Mon Oct 06 20:34:28 2014
 
 @author: DannyB648
-Version: 0.2
+Version: 0.3
 
 Purpose of this script is to play games of Peg Solitaire randomly until it successfully
 completes the game. It will then print out the successful game moves to a .txt file.
-
-
-
-STARTING PEG POSITION
-   <---------x--------->
- ^ [2][2][1][1][1][2][2]
- | [2][2][1][1][1][2][2]
- | [1][1][1][1][1][1][1]
- y [1][1][1][0][1][1][1]
- | [1][1][1][1][1][1][1]
- | [2][2][1][1][1][2][2]
- v [2][2][1][1][1][2][2]
-
-
 """
-import numpy
+
+import random
+import time
 import sys
 
-gameBoard = numpy.array(range(49)).reshape((7, 7))
-moveList = numpy.array(range(132)).reshape((33,4))
+"""
+Variable Declarations
+"""
+sys.setrecursionlimit(30000)
 
-x = 0 #X coord on board
-y = 0 #Y coord on board
-a = 0 #Interation on move list array
-movePickedX = 0
-movePickedY = 0
-checkHorizontally = True
-game = 0
-textFile = ""
-# Key for gameBoard Array: 0 = Empty, 1 = Peg, 2 = Void.
+movesPossible = 0
+
+yPicked = 0
+xPicked = 0
+dPicked = 0
+
+move = 0
+
+gameBoard = [[9,9,0,0,0,9,9,],\
+                 [9,9,0,0,0,9,9,],\
+                 [0,0,1,0,0,0,0,],\
+                 [0,0,1,1,1,0,0,],\
+                 [0,0,1,0,0,0,0,],\
+                 [9,9,0,0,0,9,9,],\
+                 [9,9,0,0,0,9,9,]]
+
+movesList = [[9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9]]
+
+"""
+Functions
+
+"""
+
 def resetBoard():
-    #global textFile   #Variable defined as you use it, so no need to pre-define textFile.
-    textFile = open("game %d.txt" % game, "w")
-    gameBoard[0][0] = 2
-    gameBoard[0][1] = 2
-    gameBoard[0][2] = 1
-    gameBoard[0][3] = 1
-    gameBoard[0][4] = 1
-    gameBoard[0][5] = 2
-    gameBoard[0][6] = 2
-    gameBoard[1][0] = 2
-    gameBoard[1][1] = 2
-    gameBoard[1][2] = 1
-    gameBoard[1][3] = 1
-    gameBoard[1][4] = 1
-    gameBoard[1][5] = 2
-    gameBoard[1][6] = 2
-    gameBoard[2][0] = 1
-    gameBoard[2][1] = 1
-    gameBoard[2][2] = 1
-    gameBoard[2][3] = 1
-    gameBoard[2][4] = 1
-    gameBoard[2][5] = 1
-    gameBoard[2][6] = 1
-    gameBoard[3][0] = 1
-    gameBoard[3][1] = 1
-    gameBoard[3][2] = 1
-    gameBoard[3][3] = 0
-    gameBoard[3][4] = 1
-    gameBoard[3][5] = 1
-    gameBoard[3][6] = 1
-    gameBoard[4][0] = 1
-    gameBoard[4][1] = 1
-    gameBoard[4][2] = 1
-    gameBoard[4][3] = 1
-    gameBoard[4][4] = 1
-    gameBoard[4][5] = 1
-    gameBoard[4][6] = 1
-    gameBoard[5][0] = 2
-    gameBoard[5][1] = 2
-    gameBoard[5][2] = 1
-    gameBoard[5][3] = 1
-    gameBoard[5][4] = 1
-    gameBoard[5][5] = 2
-    gameBoard[5][6] = 2
-    gameBoard[6][0] = 2
-    gameBoard[6][1] = 2
-    gameBoard[6][2] = 1
-    gameBoard[6][3] = 1
-    gameBoard[6][4] = 1
-    gameBoard[6][5] = 2
-    gameBoard[6][6] = 2
-    return textFile
-    
-def possibleMoves():
 
-    global x
-    global y
-    global a
-    global checkHorizontally
-    x = 0
-    y = 0
-    checkHorizontally = True
-    while x < 7 and y < 7: #This allows x and/or y to become >6 at some point, and you have an array from [0] to [6]. Will cause "index out of range."
-        print x
-        print y
-        checkHorizontally = True
-        if(x + 2 == 7):
-            y = y + 1
-        else:
-            if(gameBoard[x][y] == 1 and gameBoard[x + 1][y] == 1 and gameBoard[x + 2][y] == 0):  #Not checking if x+1 or x+2 or y+1 or y+2 is < 6. Index out of range.
-                saveMoveLeft()
-                x = x + 1
-            elif(gameBoard[x][y] == 0 and gameBoard[x + 1][y] == 1 and gameBoard[x + 2][y] == 1):
-                saveMoveRight()
-                x = x + 1
-            else:
-                x = x + 1
-    while(x < 7 and y < 7): #same as above.
-        checkHorizontally = False
-        if(y + 2 == 7):
+    gameBoard = [[9,9,1,1,1,9,9,],\
+                 [9,9,1,1,1,9,9,],\
+                 [1,1,1,1,1,1,1,],\
+                 [1,1,1,0,1,1,1,],\
+                 [1,1,1,1,1,1,1,],\
+                 [9,9,1,1,1,9,9,],\
+                 [9,9,1,1,1,9,9,]]   
+    return gameBoard
+
+def resetMovesList():
+    movesList = [[9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9],\
+                 [9,9,9]]
+
+def checkMoves(movesPossible, gameBoard, movesList):
+    for y in range(0,7):
+        for x in range(0,7):
+            if(x + 2 < 7 and gameBoard[y][x] == 1 and gameBoard[y][x + 1] == 1 and gameBoard[y][x + 2] == 0):
+                movesList[movesPossible][1] = y
+                movesList[movesPossible][2] = x
+                movesList[movesPossible][0] = 'h'
+                movesPossible = movesPossible + 1
+            elif(x + 2 < 7 and gameBoard[y][x] == 0 and gameBoard[y][x + 1] == 1 and gameBoard[y][x + 2] == 1):
+                movesList[movesPossible][1] = y
+                movesList[movesPossible][2] = x
+                movesList[movesPossible][0] = 'h'
+                movesPossible = movesPossible + 1
             x = x + 1
-        else:
-            if(gameBoard[x][y] == 1 and gameBoard[x][y + 1] == 1 and gameBoard[x][y + 2] == 0):
-                saveMoveDown()
-                y = y + 1
-            elif(gameBoard[x][y] == 0 and gameBoard[x][y + 1] == 1 and gameBoard[x][y + 2] == 1):
-                saveMoveUp()
-                y = y + 1  
-            else:
-                y = y + 1
+        y = y + 1
+        
+    for y in range(0,5):
+        for x in range(0,5):
+            if(y + 2 < 7 and gameBoard[y][x] == 1 and gameBoard[y + 1][x] == 1 and gameBoard[y + 2][x] == 0):
+                movesList[movesPossible][1] = y
+                movesList[movesPossible][2] = x
+                movesList[movesPossible][0] = 'v'
+                movesPossible = movesPossible + 1
+            elif(y + 2 < 7 and gameBoard[y][x] == 0 and gameBoard[y + 1][x] == 1 and gameBoard[y + 2][x] == 1):
+                movesList[movesPossible][1] = y
+                movesList[movesPossible][2] = x
+                movesList[movesPossible][0] = 'v'
+                movesPossible = movesPossible + 1
+            x = x + 1
+        y = y + 1
+    
+    return movesPossible, gameBoard, movesList
 
-def saveMoveLeft():
-    global x
-    global y
-    global a
-    global checkHorizontally
-    moveList[a][0] = x
-    moveList[a][1] = y
-    if(checkHorizontally == True):
-        moveList[a][2] = True
+def pickMove(movesPossible, movesList, yPicked, xPicked, dPicked, move):
+    move = random.randint(0, movesPossible - 1)
+    yPicked = movesList[move][1]
+    xPicked = movesList[move][2]
+    dPicked = movesList[move][0]
+    movesPossible = 0
+    return movesPossible, movesList, yPicked, xPicked, dPicked, move
+
+def executeMove(gameBoard, movesList, yPicked, xPicked, dPicked, move, pyPegResults):
+    xPicked2 = xPicked + 2
+    yPicked2 = yPicked + 2
+    xPickedStr = str(xPicked)
+    yPickedStr = str(yPicked)
+    yPicked2Str = str(yPicked2)
+    xPicked2Str = str(xPicked2)
+    if(movesList[move][0] == "h"):
+        if(gameBoard[yPicked][xPicked] == 0 and gameBoard[yPicked][xPicked + 1] == 1 and gameBoard[yPicked][xPicked + 2] == 1):
+            gameBoard[yPicked][xPicked] = 1
+            gameBoard[yPicked][xPicked + 1] = 0
+            gameBoard[yPicked][xPicked + 2] = 0
+            pyPegResults.write("Move from ")
+            pyPegResults.write(yPickedStr)
+            pyPegResults.write(" , ")
+            pyPegResults.write(xPicked2Str)
+            pyPegResults.write(" to ")
+            pyPegResults.write(yPickedStr)
+            pyPegResults.write(" , ")
+            pyPegResults.write(xPickedStr)
+            pyPegResults.write("\n")
+        if(gameBoard[yPicked][xPicked] == 1 and gameBoard[yPicked][xPicked + 1] == 1 and gameBoard[yPicked][xPicked + 2] == 0):
+            gameBoard[yPicked][xPicked] = 0
+            gameBoard[yPicked][xPicked + 1] = 0
+            gameBoard[yPicked][xPicked + 2] = 1
+            pyPegResults.write("Move from ")
+            pyPegResults.write(yPickedStr)
+            pyPegResults.write(" , ")
+            pyPegResults.write(xPickedStr)
+            pyPegResults.write(" to ")
+            pyPegResults.write(xPicked2Str)
+            pyPegResults.write(" , ")
+            pyPegResults.write(yPickedStr)
+            pyPegResults.write("\n")
+    elif(movesList[move][0] == "v"):
+        if(gameBoard[yPicked][xPicked] == 0 and gameBoard[yPicked + 1][xPicked] == 1 and gameBoard[yPicked + 2][xPicked] == 1):
+            gameBoard[yPicked][xPicked] = 1
+            gameBoard[yPicked + 1][xPicked] = 0
+            gameBoard[yPicked + 2][xPicked] = 0
+            pyPegResults.write("Move from ")
+            pyPegResults.write(yPicked2Str)
+            pyPegResults.write(" , ")
+            pyPegResults.write(xPickedStr)
+            pyPegResults.write(" to ")
+            pyPegResults.write(yPickedStr)
+            pyPegResults.write(" , ")
+            pyPegResults.write(xPickedStr)
+            pyPegResults.write("\n")
+        if(gameBoard[yPicked][xPicked] == 1 and gameBoard[yPicked + 1][xPicked] == 1 and gameBoard[yPicked + 2][xPicked] == 0):
+            gameBoard[yPicked][xPicked] = 0
+            gameBoard[yPicked + 1][xPicked] = 0
+            gameBoard[yPicked + 2][xPicked] = 1
+            pyPegResults.write("Move from ")
+            pyPegResults.write(yPickedStr)
+            pyPegResults.write(" , ")
+            pyPegResults.write(xPickedStr)
+            pyPegResults.write(" to ")
+            pyPegResults.write(yPicked2Str)
+            pyPegResults.write(" , ")
+            pyPegResults.write(xPickedStr)
+            pyPegResults.write("\n")
     else:
-        moveList[a][2] = False
-    a = a + 1
-    
-def saveMoveRight():
-    global x
-    global y
-    global a
-    global checkHorizontally
-    moveList[a][0] = x
-    moveList[a][1] = y
-    if(checkHorizontally == True):
-        moveList[a][2] = True
-    else:
-        moveList[a][2] = False
-    a = a + 1
-    
-def saveMoveUp():
-    global x
-    global y
-    global a
-    global checkHorizontally
-    moveList[a][0] = x
-    moveList[a][1] = y
-    if(checkHorizontally == True):
-        moveList[a][2] = True
-    else:
-        moveList[a][2] = False
-    a = a + 1
-    
-def saveMoveDown():
-    global x
-    global y
-    global a
-    global checkHorizontally
-    moveList[a][0] = x
-    moveList[a][1] = y
-    if(checkHorizontally == True):
-        moveList[a][2] = 1
-    else:
-        moveList[a][2] = 0
-    a = a + 1
-    moveList
-    
-def pickMove():
-    randomMove = 0
-    global movePickedX
-    global movePickedY
-    global a
+        print "ERROR, Neither V nor H in MoveList!"
+    return gameBoard, movesList, yPicked, xPicked, dPicked, move, pyPegResults
 
-    if a > 0:
-        randomMove = numpy.random.random_integers(a) #pick random number.
 
-    movePickedX = moveList[randomMove][0]
-    movePickedY = moveList[randomMove][1]
-    
-def executeMove(textFile):
-#    global textFile
-    if(moveList[a][2] == True):
-        if(moveList[a][3] == True):
-             gameBoard[movePickedX][movePickedY] = 0
-             gameBoard[movePickedX + 1][movePickedY] = 0
-             gameBoard[movePickedX + 2][movePickedY] = 1
-             textFile.write("X: %d , Y: %d to X: %d , Y: %d \n" %(movePickedX, movePickedY, movePickedX + 2, movePickedY))
-
-        if(moveList[a][3] == False):
-             gameBoard[movePickedX][movePickedY] = 1
-             gameBoard[movePickedX + 1][movePickedY] = 0
-             gameBoard[movePickedX + 2][movePickedY] = 0
-             textFile.write("X: %d , Y: %d to X: %d , Y: %d \n" %(movePickedX + 2, movePickedY, movePickedX, movePickedY))
-
-    if(moveList[a][2] == False):
-        if(moveList[a][3] == False):
-             gameBoard[movePickedX][movePickedY] = 0
-             gameBoard[movePickedX + 1][movePickedY] = 0
-             gameBoard[movePickedX + 2][movePickedY] = 1
-             textFile.write("X: %d , Y: %d to X: %d , Y: %d \n" %(movePickedX, movePickedY, movePickedX, movePickedY + 2))
-
-        if(moveList[a][3] == True):
-             gameBoard[movePickedX][movePickedY] = 1
-             gameBoard[movePickedX + 1][movePickedY] = 0
-             gameBoard[movePickedX + 2][movePickedY] = 0
-             textFile.write("X: %d , Y: %d to X: %d , Y: %d \n" %(movePickedX, movePickedY + 2, movePickedX, movePickedY))
-
-def evaluate(textFile):
-    global x
-    global y
-    #global textFile
+def evaluateBoard(gameBoard, pegSolved, pegsLeft):
     pegsLeft = 0
-    x = 0
-    y = 0
-    for i in xrange(0,49):
-        if(gameBoard[x][y] == 1):
-            pegsLeft = pegsLeft + 1
-        if(x < 6):
-            x = x + 1
-        if(x == 6):
-            y = y + 1
-            x = 0
-    if(pegsLeft == 1):
-        if(gameBoard[3][3] == 1):
-            textFile.write("SUCCESS! I have solved the game of Peg Solitare!")
-            sys.exit()
+    for a in range(0,7):
+        for b in range(0,7):
+            if(gameBoard[a][b] == 1):
+                pegsLeft = pegsLeft + 1
+            b = b + 1
+        a = a + 1
+    if(pegsLeft == 1 and gameBoard[3][3] == 1):
+        pegSolved == True
+    return gameBoard, pegSolved, pegsLeft
+        
+
+"""
+Actual Sequence
+"""
+
+
+pegSolved = False
+gameFinished = False
+pegsLeft = 0
+pyPegResults = open("pyPegResults.txt","w")
+gamesPlayed = 0
+
+def play(pegSolved, gameFinished, movesPossible, gameBoard, movesList, yPicked, xPicked, dPicked, move, pegsLeft, pyPegResults, gamesPlayed):
+    while pegSolved == False:
+        pyPegResults = open("pyPegResults.txt","w")
+        #print"Games Played: ", gamesPlayed
+        gameBoard = resetBoard()
+        resetMovesList()
+        gameFinished = False
+        while gameFinished == False:
+            movesPossible, gamesBoard, movesList = checkMoves(movesPossible, gameBoard, movesList)
+            if(movesPossible == 0):
+                
+                gameFinished = True
+                break
+            movesPossible, movesList, yPicked, xPicked, dPicked, move = pickMove(movesPossible, movesList, yPicked, xPicked, dPicked, move)
+            gameBoard, movesList, yPicked, xPicked, dPicked, move, pyPegResults = executeMove(gameBoard, movesList, yPicked, xPicked, dPicked, move, pyPegResults)  
+        gameBoard, pegSolved, pegsLeft = evaluateBoard(gameBoard, pegSolved, pegsLeft)
+        time.sleep(0.00000000000000000000001)
+
+        if(pegsLeft == 1 and gameBoard[3][3] == 1):
+            print"SUCCESS"
+            pyPegResults.write("Game Won!")
+            pyPegResults.write("\n I've Played ")
+            gamesPlayed = gamesPlayed + 1
+            gamesPlayedStr = str(gamesPlayed)
+            pyPegResults.write(gamesPlayedStr)
+            pyPegResults.write(" Games before I solved it!")
+            pyPegResults.close()
+            pegSolved = True
+            break
         else:
-            playGame()
-    else:
-        playRound()        
-            
-
-    
-def playRound(textFile):
-
-    possibleMoves()
-    pickMove()
-    executeMove(textFile)
-    evaluate(textFile)
-    
-def playGame(game):
-    game = game + 1
-    txtFile = resetBoard() #variable txtFile now equal to return value of resetBoard. return should be a file object.
-    playRound(txtFile)
-    
-playGame(game)
+            pyPegResults.write("Game Lost!")
+            pyPegResults.write("\n I've Played ")
+            gamesPlayed = gamesPlayed + 1
+            gamesPlayedStr = str(gamesPlayed)
+            pyPegResults.write(gamesPlayedStr)
+            pyPegResults.write(" Games so far!")
+            pyPegResults.close()
+            #print gameBoard
+            #play(pegSolved, gameFinished, movesPossible, gameBoard, movesList, yPicked, xPicked, dPicked, move, pegsLeft, pyPegResults, gamesPlayed)
+    print "SUCCESS! Check how to Solve Peg Solitare!"
+    return pegSolved
+while pegSolved == False: #DEBUG
+    if(gamesPlayed == 1000):
+        print"I've only played 1000 games so far!
+    elif(gamesPlayed == 10000):
+        print"I've played 10000 games now!"
+    elif(gamesPlayed == 50000):
+        print"I've played 50K games and im not done yet!"
+    elif(gamesPlayed%10000 == 0 and gamesPlayed > 50000):
+        print"Ive played ",gamesPlayed" so far and i'm getting bored!"
+        print gameBoard
+    pegSolved = play(pegSolved, gameFinished, movesPossible, gameBoard, movesList, yPicked, xPicked, dPicked, move, pegsLeft, pyPegResults, gamesPlayed)
